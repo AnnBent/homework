ea8f5dee3401f5316acca6f9a726058fa7b0cc71
AnnBent <anna.fedina.v@gmail.com>
2022-10-20 16:56:37 +0300

    completed lesson5

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+n1 = 456
+n2 = -18
+print("integer:", n1, ",", n2)
+
+n3 = 4.75
+n4 = -4.00
+print("float:", n3, ",", n4)
+
+n5 = True
+n6 = False
+print("boolean:", n5, ",", n6)
+
+n7 = "Hello"
+n8 = "World"
+print("str:", n7, n8)
+
+n9 = None
+print("none:", n9)
