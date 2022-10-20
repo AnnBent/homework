65375d4e8f28dfff25ad84f5e540ca45355434d5
AnnBent <anna.fedina.v@gmail.com>
2022-10-20 17:08:00 +0300

    completed lesson9

---
+++ lesson9.py
@@ -0,0 +1,9 @@
+text = 'Сделай сегодня то, за что сам потом скажешь спасибо'
+
+print('1. ', text)
+print('2. ', text[1::2])
+print('3. ', text[::2])
+print('4. ', text[::-1])
+print('5. ', text[6:])
+print('6. ', text[:6])
+print('7. ', text[49:3:-1])
