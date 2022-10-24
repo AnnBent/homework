5183b3b68aaa478d77f9aa77ef0cba97ef71eca5
AnnBent <anna.fedina.v@gmail.com>
2022-10-24 20:35:16 +0300

    completed lesson12_1

--- lesson12_1.py
+++ lesson12_1.py
@@ -1,8 +1,11 @@
 try:
     chislo = int(input("Введите пятизначное натуральное число: "))
+    max_chislo = 0
     if 9999 < chislo < 100000:
-        print("ok")
-        for i in range()
+        for i in range(0, 5):
+            if int(str(chislo)[i]) > max_chislo:
+                max_chislo = int(str(chislo)[i])
+        print("Наибольшая цифра этого числа равна ", max_chislo)
     else:
         print("Вы ввели не пятизначное число")
 except ValueError:
