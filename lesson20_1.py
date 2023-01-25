c45d05c363e50cc8e9c451d86c83975aea08ec75
AnnBent <anna.fedina.v@gmail.com>
2023-01-25 14:18:22 +0300

    completed lesson20_1

--- lesson20_1.py
+++ lesson20_1.py
@@ -1,8 +1,8 @@
 import re
+counter = {}
 str1 = "Мы любим, но делаем вид, что нам безразлично. Мы равнодушны, а делаем вид, что любим."
 list1 = re.split("\. | |, |\.|\n", str1)
 print(str1)
-set1 = list1
-for i in set1:
-    print(i, "-", list1.count(i))
-
+for i in list1:
+    counter[i] = counter.get(i, 0) + 1
+    print(i, "-", counter[i] - 1)
