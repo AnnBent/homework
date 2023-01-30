89c17fccd18a0c6f486ace0b424a0d4fb96ed1a1
AnnBent <anna.fedina.v@gmail.com>
2023-01-30 15:30:11 +0300

    completed lesson21_3

---
+++ lesson21_3.py
@@ -0,0 +1,10 @@
+with open("text.txt", 'r') as f:
+    word = symbol = line_count = 0
+    for line in f:
+        print(line.rstrip())
+        line_count += 1
+        symbol = len(line)
+        line = line.split()
+        word = len(line)
+        print("Количество символов в строке: ", symbol,"Слов:" , word)
+    print("Количество строк в тексте: ", line_count)
